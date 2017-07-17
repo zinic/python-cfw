7426bfee9ef24a9e2700374ab1cf882f722b0419
John Hopper <john.hopper@jpserver.net>
2017-07-17 09:41:01 -0700

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cfw',
-    version='0.1.8',
+    version='0.1.9',
     url='https://github.com/zinic/python-cfw',
     description='Python Command Line Interface Library',
 
